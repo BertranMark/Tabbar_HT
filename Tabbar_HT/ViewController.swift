883822070fbb6bb7ce059699b65068ef04bd8d9d
BertranMark <37067020+BertranMark@users.noreply.github.com>
2018-09-03 12:45:32 +0300

    Initial Commit

---
+++ Tabbar_HT/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tabbar_HT
+//
+//  Created by Bertran on 03.09.2018.
+//  Copyright © 2018 Bertran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
